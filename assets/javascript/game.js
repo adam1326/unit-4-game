f6cfa7215227bdbcfa4369c8f91d847c01d8b121
adam1326 <adam.mccain@gmail.com>
2018-04-02 03:54:11 -0400

    added new background and fixed html and some js

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+//Start of Crystal game
+
+
+
+//variables and arrays 
+wins = 0;
+losses = 0;
+randomNumber = 0;
+yourNumber = 0;
+crystalRandom = "";
+
+
+//Random number the user must meet
+
+
+//Random number for each crystal
+
+
+//win and losses
+
+
+
+//total score
