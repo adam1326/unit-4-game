668d9f4c2eff555cd7d6767dd656efdf3d9c4879
adam1326 <adam.mccain@gmail.com>
2018-04-03 05:34:36 -0400

    added some js code and fixed centering of page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,32 @@
 //Start of Crystal game
 
 
-
 //variables and arrays 
-wins = 0;
-losses = 0;
-randomNumber = 0;
-yourNumber = 0;
-crystalRandom = "";
+var wins = 0;
+var losses = 0;
+var randomNumber = 0;
+var yourNumber = 0;
+var crystalRandom = "";
 
 
 //Random number the user must meet
+$(document).ready(function(){
+    var Random=Math.floor(Math.random()*101+19)
+    console.log(Random)
+    $('#random').text(Random);
 
+});
 
 //Random number for each crystal
 
+gem.on("click", "#gem", function() {
+    var numberOptions=Math.floor(Math.random()*12-1)
+    console.log(numberOptions)
+    $('#gem').image(numberOptions);
+
+});
 
 //win and losses
 
 
-
-//total score
+//Total Score
